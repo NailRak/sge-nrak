7b631ef0585c47394720588d6989e4c97f1ad6e5
saul <bichito@gmail.com>
2026-06-09 19:29:03 -0300

    terminamos tp10

--- src/Controller/Admin/AdminEventoController.php
+++ src/Controller/Admin/AdminEventoController.php
@@ -6,6 +6,7 @@ use App\Repository\EventoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/admin/evento')]
 class AdminEventoController extends AbstractController
@@ -27,4 +28,59 @@ class AdminEventoController extends AbstractController
             ]
         );
     }
+    #[Route(
+    '/inscriptos/{id}',
+    name: 'admin_evento_inscriptos',
+    requirements: ['id' => '\d+']
+)]
+public function inscriptos(
+    int $id,
+    EventoRepository $eventoRepository
+): Response
+{
+    $evento = $eventoRepository->find($id);
+
+    if (!$evento) {
+        throw $this->createNotFoundException(
+            'Evento no encontrado'
+        );
+    }
+
+    return $this->render(
+        'admin/evento/inscriptos.html.twig',
+        [
+            'evento' => $evento
+        ]
+    );
+}
+#[Route(
+    '/borrar/{id}',
+    name: 'admin_evento_borrar',
+    requirements: ['id' => '\d+']
+)]
+public function borrar(
+    int $id,
+    EventoRepository $eventoRepository,
+    EntityManagerInterface $entityManager
+): Response
+{
+    $evento = $eventoRepository->find($id);
+
+    if (!$evento) {
+        throw $this->createNotFoundException(
+            'No existe el evento solicitado.'
+        );
+    }
+
+    $entityManager->remove($evento);
+
+    $entityManager->flush();
+
+    return $this->render(
+        'admin/evento/borrar.html.twig',
+        [
+            'evento' => $evento
+        ]
+    );
+}
 }
